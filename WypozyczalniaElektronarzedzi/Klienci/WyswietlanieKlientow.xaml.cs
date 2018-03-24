ba6a1e39fbce5281b4a0a56c6f9d277e4620e726
Adam Mitręga <amitrega01@gmail.com>
2018-03-24 11:37:05 +0100

    dodano: wypozyczenia

---
+++ WypozyczalniaElektronarzedzi/Klienci/WyswietlanieKlientow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using ModelBazy;
+namespace WypozyczalniaElektronarzedzi
+{   
+    public partial class WyswietlanieKlientow : UserControl
+    {   
+        List<Klienci> klienciRes;
+        public WyswietlanieKlientow()
+        {
+            InitializeComponent();
+            klienciRes = new List<Klienci>();
+            UpdateUI();
+        }
+        /// <summary>
+        /// Odswieża interfejs i pobiera dane z bazy na nowo
+        /// </summary>
+        public void UpdateUI()
+        {
+            using (var context = new WypozyczalniaEntities()) 
+            {
+                klienciRes = context.Klienci.ToList();
+            }
+            KlienciGrid.ItemsSource = klienciRes;
+        }
+
+        private void OdwiezBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateUI();
+        }
+    }
+}
---
+++ WypozyczalniaElektronarzedzi/Wypozyczenia/WypozyczenieDodaj.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ModelBazy;
+
+namespace WypozyczalniaElektronarzedzi
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WypozyczenieDodaj.xaml
+    /// </summary>
+    public partial class WypozyczenieDodaj : UserControl
+    {
+        public WypozyczenieDodaj()
+        {
+            InitializeComponent();
+            UpdateUI();
+        }
+        public void UpdateUI()
+        {
+            using (var context = new WypozyczalniaEntities())
+            {
+                ProduktyLB.ItemsSource = context.PelnyProdukt.ToList();
+                
+            }
+        }
+    }
+}
